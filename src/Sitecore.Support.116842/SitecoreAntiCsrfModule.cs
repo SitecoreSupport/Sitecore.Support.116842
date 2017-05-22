43661fae2bbf5d8c3daa48693fe34592adcc6e92
DK\ili <ili@sitecore.net>
2017-05-22 17:01:18 +0300

    no message

--- src/Sitecore.Support.116842/SitecoreAntiCsrfModule.cs
+++ src/Sitecore.Support.116842/SitecoreAntiCsrfModule.cs
@@ -5,11 +5,9 @@ using System.Web;
 
 namespace Sitecore.Support.Security.AntiCsrf
 {
-<<<<<<< HEAD
-    public class SitecoreAntiCsrfModule : Sitecore.Security.AntiCsrf.SitecoreAntiCsrfModule
-=======
+
     public class SitecoreAntiCsrfModule: Sitecore.Security.AntiCsrf.SitecoreAntiCsrfModule
->>>>>>> e9319a6b7b10c2e1c2832a5d23a7028262418609
+
     {
         internal void InvokeBaseLoadConfiguration()
         {
