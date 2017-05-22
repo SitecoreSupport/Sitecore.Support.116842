2f89d02ea5b916271600b34b31ea1cbcc661ffce
DK\ili <ili@sitecore.net>
2017-05-22 15:54:50 +0300

    no message

---
+++ src/Sitecore.Support.116842/SitecoreAntiCsrfModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Security.AntiCsrf
+{
+    public class SitecoreAntiCsrfModule : Sitecore.Security.AntiCsrf.SitecoreAntiCsrfModule
+    {
+        internal void InvokeBaseLoadConfiguration()
+        {
+            base.LoadConfiguration();
+        }
+    }
+}
